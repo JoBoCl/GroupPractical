ab5cdde04e518b9cae363aec251c04186798b5c1
James Wallis <shroom.monk@googlemail.com>
2014-03-30 18:28:15 +0100

    Updated specification for GPSService.

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/GPSService.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/GPSService.java
@@ -24,4 +24,10 @@ public class GPSService {
 	 */
 	public double getLatitude() { return 0.0d; }
 	public double getLongitude() { return 0.0d; }
+	
+	/*
+	 * Calculate the distance between two world coordinates, in km
+	 */
+	public double getDistanceBetween(double lat1, double long1, double lat2, double long2) {
+	}
 }
