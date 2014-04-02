ac4bafdb884c41b749abcb5507c1db6ec8be1ab6
James Wallis <shroom.monk@googlemail.com>
2014-04-02 17:03:47 +0100

    Implemented representation of OpeningHours class.
    Note constructors are debug-only - not final.

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/OpeningHours.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/OpeningHours.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.cs.GPT9.augox;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Represents the times that some place is open. Given that there are complex
@@ -8,6 +9,28 @@ import java.util.Date;
  * is provided.
  */
 public class OpeningHours {
+	/*
+	 * Representation:
+	 * List of periods in priority order (highest first).
+	 * Each period represents a recurring block of time, and whether the thing
+	 * is open/closed during that block.
+	 * 
+	 * Other notes:
+	 * All times UTC.
+	 */
+	
+	/*
+	 * Variables
+	 */
+	private List<Period> periods;
+	
+	/*
+	 * Constructor
+	 */
+	public OpeningHours(List<Period> periods) {
+		this.periods = periods;
+	}
+	
 	/*
 	 * Return if the thing is open at give time and date.
 	 */
@@ -24,4 +47,40 @@ public class OpeningHours {
 	 * Return a summary of the opening times as a human-readable String.
 	 */
 	public String getOpeningSummary() { return ""; }
+	
+	/*
+	 * Period Class
+	 */
+	public class Period {
+		/*
+		 * Variables
+		 */
+		private boolean hasGeneralSpan;		// False: applies all year
+		private int startMonth, startDay;	// hasGeneralSpan => set these to
+		private int endMonth, endDay;		// the general period this applies
+		private boolean[] daysApplied;		// Day names this period applies on
+		private boolean isOpen;				// False: closed over this period
+		private int openHour, openMinute;	// isOpen => set these to opening
+		private int closeHour, closeMinute;	// and closing times in this period
+		
+		/*
+		 * Constructor
+		 */
+		public Period(	boolean hasGeneralSpan, int startMonth, int startDay,
+						int endMonth, int endDay, boolean[] daysApplied,
+						boolean isOpen, int openHour, int openMinute,
+						int closeHour, int closeMinute	) {
+			this.hasGeneralSpan = hasGeneralSpan;
+			this.startMonth = startMonth;
+			this.startDay = startDay;
+			this.endMonth = endMonth;
+			this.endDay = endDay;
+			this.daysApplied = daysApplied;
+			this.isOpen = isOpen;
+			this.openHour = openHour;
+			this.openMinute = openMinute;
+			this.closeHour = closeHour;
+			this.closeMinute = closeMinute;
+		}
+	}
 }
