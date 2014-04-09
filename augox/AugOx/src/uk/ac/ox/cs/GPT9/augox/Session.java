843619ac349889d4aa27db2632a031d996936535
Joshua Clark <joshuaclark94@gmail.com>
2014-04-09 16:57:02 +0100

    Added enum and helper class for sessions

---
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/Session.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package uk.ac.ox.cs.GPT9.augox;
+
+/**
+ * @author Joshua Allows quick differentiation between different types of
+ *         activity (This is an activity for the user, not the program)
+ */
+public enum Session {
+	BREAKFAST, MORNING, LUNCH, AFTERNOON, TEA, EVENING, NIGHT;
+}
