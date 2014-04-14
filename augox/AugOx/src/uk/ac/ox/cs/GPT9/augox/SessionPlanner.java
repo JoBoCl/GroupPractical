10f97ce3054fa22325469f894a6535c5c8e2846c
Joshua Clark <joshuaclark94@gmail.com>
2014-04-14 18:02:43 +0100

    Return sensible set of places when asked to populate gallery

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SessionPlanner.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SessionPlanner.java
@@ -42,6 +42,21 @@ public class SessionPlanner extends Fragment {
 			public void onStopTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
 
+	private PlaceData[] choosePlaces(List<PlaceData> places) {
+		PlaceData dummy = new PlaceData();
+		switch (places.size()) {
+		case 0:
+			return new PlaceData[] { dummy, dummy, dummy };
+		case 1:
+			return new PlaceData[] { places.get(0), dummy, dummy };
+		case 2:
+			return new PlaceData[] { places.get(0), places.get(1), dummy };
+		case 3:
+			return new PlaceData[] { places.get(0), places.get(1),
+					places.get(2) };
+		default:
+			return new PlaceData[] { places.get(0), places.get(1),
+					places.get(2) };
 			}
 	}
 
