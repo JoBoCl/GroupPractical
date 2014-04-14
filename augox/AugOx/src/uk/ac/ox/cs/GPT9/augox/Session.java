cd278fbffbae5d21138e7c94649feedbe3c01f46
Joshua Clark <joshuaclark94@gmail.com>
2014-04-14 17:52:30 +0100

    Amended session enum to allow sensible call toString

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/Session.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/Session.java
@@ -8,5 +8,25 @@ package uk.ac.ox.cs.GPT9.augox;
  *         activity (This is an activity for the user, not the program)
  */
 public enum Session {
-	BREAKFAST, MORNING, LUNCH, AFTERNOON, TEA, EVENING, NIGHT;
+	BREAKFAST ("Breakfast"),
+    MORNING ("Morning"),
+    LUNCH ("Lunch"),
+    AFTERNOON ("Afternoon"),
+    TEA ("Tea"),
+    EVENING ("Evening"),
+    NIGHT ("Night");
+
+    private final String name;       
+
+    private Session(String s) {
+        name = s;
+    }
+
+    public boolean equalsName(String otherName){
+        return (otherName == null)? false:name.equals(otherName);
+    }
+
+    public String toString(){
+       return name;
+    }
 }
