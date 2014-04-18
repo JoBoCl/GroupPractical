0faef847fbf671139c7aa99d9670df9016e26605
James Wallis <shroom.monk@googlemail.com>
2014-04-18 17:24:54 +0100

    Test OSM fetching code.

--- augox/AugOxDatabaseTool/src/uk/ac/ox/cs/GPT9/augox/databasetool/MainActivity.java
+++ augox/AugOxDatabaseTool/src/uk/ac/ox/cs/GPT9/augox/databasetool/MainActivity.java
@@ -2,6 +2,8 @@ package uk.ac.ox.cs.GPT9.augox.databasetool;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.URL;
 
 import android.app.Activity;
@@ -28,7 +30,9 @@ public class MainActivity extends Activity {
 	}
 	
 	public void fetchData(View view) {
-		new FetchInternetTask().execute("http://www.google.co.uk/");
+		String apihost = "http://api.openstreetmap.org/api/0.6/";
+		String apicommand = "map?bbox=-1.2605,51.76,-1.26,51.7605";
+		new FetchInternetTask().execute(apihost + apicommand);
 	}
 	
 	private class FetchInternetTask extends AsyncTask<String, Void, String> {
@@ -36,10 +40,12 @@ public class MainActivity extends Activity {
 			String input, result = "";
 			
 			try {
+				//Authenticator.setDefault(new OSMAuthenticator());
 				URL url = new URL(urls[0]);
 				BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 				while((input = in.readLine()) != null) {
-					result += input;
+					Log.d("James_DBG", input);
+					result += input + "\n";
 				}
 				in.close();
 			} catch (Exception e) {
@@ -54,6 +60,14 @@ public class MainActivity extends Activity {
 		protected void onPostExecute(String result) {
 			TextView outputpane = (TextView) findViewById(R.id.outputpane);
 			outputpane.setText(result);
+		}
+		
+		private class OSMAuthenticator extends Authenticator {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				String username = "oxfordgrouppracticalteam9@gmail.com";
+				String password = "IDon'tCare";
+				return new PasswordAuthentication(username, password.toCharArray());				
+			}
 		}
 	}
 
