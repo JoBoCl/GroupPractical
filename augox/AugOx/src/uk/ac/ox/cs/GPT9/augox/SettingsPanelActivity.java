1f043805bd60cebcf8a70035bdd6f06889f9823c
Stephen <stephenheap1@hotmail.com>
2014-05-05 17:36:38 +0100

    Settings Screen completed
    Only two settings so far.

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SettingsPanelActivity.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SettingsPanelActivity.java
@@ -10,7 +10,6 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -18,7 +17,6 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
-import android.text.InputType;
 import android.text.TextUtils;
 import android.widget.Toast;
 
@@ -61,10 +59,6 @@ public class SettingsPanelActivity extends PreferenceActivity {
 		Toast.makeText(this, "testing", Toast.LENGTH_LONG).show();
 		getFragmentManager().beginTransaction().replace(android.R.id.content, 
 				new GeneralPreferenceFragment()).commit();
-		getFragmentManager().beginTransaction().replace(android.R.id.content, 
-				new NotificationPreferenceFragment()).commit();
-		getFragmentManager().beginTransaction().replace(android.R.id.content, 
-				new DataSyncPreferenceFragment()).commit();
 		// In the simplified UI, fragments are not used at all and we instead
 		// use the older PreferenceActivity APIs.
 		
@@ -215,45 +209,6 @@ public class SettingsPanelActivity extends PreferenceActivity {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.pref_general);
 			bindPreferenceSummaryToValue(findPreference("setting_autoroute_max_length"));
-		}
-	}
-
-	/**
-	 * This fragment shows notification preferences only. It is used when the
-	 * activity is showing a two-pane settings UI.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class NotificationPreferenceFragment extends
-			PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_notification);
-
-			// Bind the summaries of EditText/List/Dialog/Ringtone preferences
-			// to their values. When their values change, their summaries are
-			// updated to reflect the new value, per the Android Design
-			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("notifications_new_message_ringtone"));
-		}
-	}
-
-	/**
-	 * This fragment shows data and sync preferences only. It is used when the
-	 * activity is showing a two-pane settings UI.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class DataSyncPreferenceFragment extends PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_data_sync);
-
-			// Bind the summaries of EditText/List/Dialog/Ringtone preferences
-			// to their values. When their values change, their summaries are
-			// updated to reflect the new value, per the Android Design
-			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("sync_frequency"));
 		}
 	}
 }
