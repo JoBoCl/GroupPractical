4481c9c397296023fad93867291fab180eeb30fa
Joshua Clark <joshuaclark94@gmail.com>
2014-05-05 17:02:25 +0100

    Joshua's ultimate commit of finally (mostly) working stuff

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/AutoPlannerActivity.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/AutoPlannerActivity.java
@@ -1,192 +1,106 @@
 package uk.ac.ox.cs.GPT9.augox;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
-import uk.ac.ox.cs.GPT9.augox.R.layout;
-import uk.ac.ox.cs.GPT9.augox.dbquery.AndQuery;
-import uk.ac.ox.cs.GPT9.augox.dbquery.CategoryQuery;
-import uk.ac.ox.cs.GPT9.augox.dbquery.DatabaseQuery;
-import uk.ac.ox.cs.GPT9.augox.dbquery.OpenAtQuery;
-
+import android.annotation.TargetApi;
 import android.os.Bundle;
-import android.app.Dialog;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.app.TimePickerDialog;
-import android.app.Activity;
-import android.text.format.DateFormat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import uk.ac.ox.cs.GPT9.augox.R;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
-public class AutoPlannerActivity extends Activity {
+@TargetApi(11)
+public class AutoPlannerActivity extends FragmentActivity {
+	private static GalleryPickerFragment[] activities;
+	private SeekBar activityCount;
+	private Button finished;
+	private int oldCount;
 
-	private Fragment[] activities;
-    private SeekBar activityCount;
-	/*
-	 * @Override protected void onCreate(Bundle savedInstanceState) {
-	 * super.onCreate(savedInstanceState);
-	 * setContentView(R.layout.activity_auto_planner); }
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.auto_planner, menu);
-		return true;
+	public static void getPlaces() {
+		ArrayList localArrayList = new ArrayList();
+		GalleryPickerFragment[] arrayOfGalleryPickerFragment = activities;
+		int i = arrayOfGalleryPickerFragment.length;
+		for (int j = 0;; j++) {
+			if (j >= i) {
+				MainScreenActivity.getCurrentRoute().setList(localArrayList);
+				return;
+			}
+			GalleryPickerFragment localGalleryPickerFragment = arrayOfGalleryPickerFragment[j];
+			if (localGalleryPickerFragment.getSelectedPlace() != null) {
+				localArrayList.add(localGalleryPickerFragment
+						.getSelectedPlace());
+			}
+		}
 	}
 
-	@Override
+	private void updateViewableActivities(int visibleActivities) {
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		for(int i = 0; i < 14; i++) {
+		if (i < visibleActivities) {
+			ft.show(activities[i]);
+			Log.d("Joshua", "Showed gallery " + Integer.toString(i));
+		}
+		else {
+			ft.hide(activities[i]);
+			Log.d("Joshua", "Hid gallery " + Integer.toString(i));
+		}
+		}
+			this.oldCount = visibleActivities;
+			ft.commit();
+	}
+
 	public void onCreate(Bundle savedInstanceState) {
 		Log.d("Joshua", "onCreate of AutoPlanner, before other stuff");
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_auto_planner);
+		setContentView(2130903040);
 
-		// startTime = (Button) findViewById(R.id.startTimeButton);
-		// endTime = (Button) findViewById(R.id.endTimeButton);
-		activities = new Fragment[getResources().getInteger(
-				R.integer.ActivityLimit)];
-		LinearLayout galleryHolders = (LinearLayout) findViewById(R.id.galleryHolders);
-		for (int i = 0; i < getResources().getInteger(R.integer.ActivityLimit); i++) {
-			activities[i] = new GalleryPickerFragment();
-		}
-
-		updateViewableActivities(0);
-		
-		activityCount = (SeekBar) findViewById(R.id.activityCount);
-		activityCount.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-			}
-			
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				updateViewableActivities(progress);
-				
-			}
-		});
-
-	}
-
-	private void updateViewableActivities(int activityCount) {
-		FragmentManager fm = getFragmentManager();
-		FragmentTransaction ft = fm.beginTransaction();
-		for (int i = 0; i < getResources().getInteger(R.integer.ActivityLimit); i++) {
-			if (i < activityCount) {
-				ft.add(i, activities[i]);
-			} else try {
-					ft.remove(activities[i]);
-				} catch (Exception e) {
-				}
+		activities = new GalleryPickerFragment[14];
+		LinearLayout galleryLayout = (LinearLayout) findViewById(2131492867);
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		Log.d("Joshua", "Before loop start");
+		for (int i = 0; i < 14; i++) {
+			activities[i] = GalleryPickerFragment.newInstance(i);
+			Log.d("Joshua", "Added fragment to activities");
+			ft.add(galleryLayout.getId(), activities[i]);
+			Log.d("Joshua", "Added fragment to linear view");
 		}
 		ft.commit();
+		Log.d("Joshua", "Committed changes");
+		this.oldCount = 0;
+		updateViewableActivities(0);
+		this.activityCount = ((SeekBar) findViewById(2131492866));
+		this.activityCount
+				.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+					public void onProgressChanged(SeekBar seekBar,
+							int newValue, boolean userChange) {
+						AutoPlannerActivity.this
+								.updateViewableActivities(newValue);
+					}
+
+					public void onStartTrackingTouch(
+							SeekBar paramAnonymousSeekBar) {
+					}
+
+					public void onStopTrackingTouch(
+							SeekBar paramAnonymousSeekBar) {
+					}
+				});
+		this.finished = ((Button) findViewById(R.id.routeFinished));
+		this.finished.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View paramAnonymousView) {
+			}
+		});
+		Log.d("Joshua", "Finished onCreate");
+		return;
 	}
 
-	/*
-	 * // display current time public void setCurrentTimeOnView() {
-	 * 
-	 * final Calendar c = Calendar.getInstance(); startHour =
-	 * c.get(Calendar.HOUR_OF_DAY); startMinute = c.get(Calendar.MINUTE);
-	 * endHour = c.get(Calendar.HOUR_OF_DAY); endMinute =
-	 * c.get(Calendar.MINUTE);
-	 * 
-	 * // set current time into textview
-	 * startTime.setText(String.format("Start time: %02d:%02d", startHour,
-	 * startMinute)); endTime.setText(String.format("End time: %02d:%02d",
-	 * startHour, startMinute)); }
-	 * 
-	 * public void addListenerOnButton() { OnClickListener listener = new
-	 * OnClickListener() {
-	 * 
-	 * @SuppressWarnings("deprecation")
-	 * 
-	 * @Override public void onClick(View v) { showDialog(v == startTime ?
-	 * START_TIME_DIALOG_ID : v == endTime ? END_TIME_DIALOG_ID : null); }
-	 * 
-	 * }; startTime.setOnClickListener(listener);
-	 * endTime.setOnClickListener(listener);
-	 * 
-	 * }
-	 * 
-	 * private void updateGalleries() { int galleries = findIntervals();
-	 * List<Session> sessions = new ArrayList<Session>(); int startTime =
-	 * startHour * 60 + startMinute; int endTime = endHour * 60 + endMinute; for
-	 * (int time : intervals) { if (startTime < time && time < endTime)
-	 * sessions.add(SessionHelper.getSessionAtTime(time)); } Session[] ss = new
-	 * Session[] { Session.BREAKFAST, Session.MORNING, Session.LUNCH,
-	 * Session.AFTERNOON, Session.TEA, Session.EVENING, Session.NIGHT,
-	 * Session.BREAKFAST, Session.MORNING, Session.LUNCH, Session.AFTERNOON,
-	 * Session.TEA, Session.EVENING, Session.NIGHT }; FragmentManager fm =
-	 * getFragmentManager(); FragmentTransaction ft = fm.beginTransaction(); int
-	 * j = 0; for (int i = 0; i < ss.length; i++) { /* PlaceCategory[] placecat
-	 * = SessionHelper.getCategoriesForSession(session); DatabaseQuery plcat =
-	 * new CategoryQuery(Arrays.asList(placecat)); DatabaseQuery open = new
-	 * OpenAtQuery( SessionHelper.getStartTimeForSession(session));
-	 * DatabaseQuery and = new AndQuery(plcat, open); List<PlaceData> places =
-	 * MainScreenActivity.getPlacesDatabase() .queryFetchPlaces(and);
-	 * PlaceData[] chosenPlaces = choosePlaces(places);
-	 *//*
-		 * Session session = ss[i]; if (j < sessions.size() && sessions.get(j)
-		 * == session) { Fragment spf = (Fragment) new SessionPlanner(session);
-		 * ft.add(R.id.sessionList, spf, "session planner " + session.toString()
-		 * + (i > ss.length / 2 ? "2" : "1")); spfs.add(spf); j++; } else {
-		 * Fragment f = getFragmentManager().findFragmentByTag(
-		 * "session planner " + session.toString() + (i > ss.length / 2 ? "2" :
-		 * "1")); if (f != null) ft.remove(f); } } ft.commit(); }
-		 * 
-		 * private int findIntervals() { int sessions = 0; int i = 0; int n =
-		 * intervals.length; int startTime = startHour * 60 + startMinute; int
-		 * endTime = endHour * 60 + endMinute; // if the end time is "before"
-		 * the start time, assume it runs until the // next day. endTime +=
-		 * endTime < startTime ? 36 * 24 : 0; while (i < n) { if (startTime <
-		 * intervals[i] && intervals[i] < endTime) sessions++; i++; } return
-		 * sessions; }
-		 * 
-		 * @Override protected Dialog onCreateDialog(int id) { switch (id) {
-		 * case START_TIME_DIALOG_ID: // set time picker as current time return
-		 * new TimePickerDialog(this, startTimePickerListener, startHour,
-		 * startMinute, false); case END_TIME_DIALOG_ID: return new
-		 * TimePickerDialog(this, endTimePickerListener, endHour, endMinute,
-		 * false); } return null; }
-		 * 
-		 * private TimePickerDialog.OnTimeSetListener endTimePickerListener =
-		 * new TimePickerDialog.OnTimeSetListener() { public void
-		 * onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
-		 * endHour = selectedHour; endMinute = selectedMinute;
-		 * 
-		 * // set // current // time // into // textview
-		 * endTime.setText(String.format("End time: %02d:%02d", selectedHour,
-		 * selectedMinute)); updateGalleries(); } };
-		 * 
-		 * private TimePickerDialog.OnTimeSetListener startTimePickerListener =
-		 * new TimePickerDialog.OnTimeSetListener() { public void
-		 * onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
-		 * startHour = selectedHour; startMinute = selectedMinute;
-		 * 
-		 * // set // current // time // into // textview
-		 * startTime.setText(String.format("Start time: %02d:%02d",
-		 * selectedHour, selectedMinute));
-		 * 
-		 * updateGalleries(); } };
-		 */
+	public boolean onCreateOptionsMenu(Menu paramMenu) {
+		getMenuInflater().inflate(2131427328, paramMenu);
+		return true;
+	}
 }
--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/GalleryPickerFragment.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/GalleryPickerFragment.java
@@ -1,134 +1,177 @@
-/**
- * 
- */
 package uk.ac.ox.cs.GPT9.augox;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.List;
 
 import uk.ac.ox.cs.GPT9.augox.dbquery.AndQuery;
 import uk.ac.ox.cs.GPT9.augox.dbquery.CategoryQuery;
-import uk.ac.ox.cs.GPT9.augox.dbquery.DatabaseQuery;
 import uk.ac.ox.cs.GPT9.augox.dbquery.OpenAtQuery;
 import uk.ac.ox.cs.GPT9.augox.dbsort.NameSorter;
 import uk.ac.ox.cs.GPT9.augox.dbsort.SortOrder;
-import android.app.Fragment;
+import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import uk.ac.ox.cs.GPT9.augox.R;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 
+@TargetApi(11)
 public class GalleryPickerFragment extends Fragment {
-
 	private RadioGroup categoryChoice;
-	private PlaceData place;
-	private PlaceData[] places = new PlaceData[3];
 	private RadioButton[] chosenPlace = new RadioButton[3];
-	private ImageView placeImage0, placeImage1, placeImage2;
+	private int lastChecked;
+	private int offset;
+	private CheckBox ownPlan;
+	private ImageView[] placeImages = new ImageView[3];
+	private PlaceData[] places = new PlaceData[3];
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.gallery_picker, container);
-
-		// Here we are fetching the layoutParams from parent activity and
-		// setting it to the fragment's view.
-
-		chosenPlace[0] = (RadioButton) getView()
-				.findViewById(R.id.chosenPlace0);
-		chosenPlace[1] = (RadioButton) getView()
-				.findViewById(R.id.chosenPlace1);
-		chosenPlace[2] = (RadioButton) getView()
-				.findViewById(R.id.chosenPlace2);
-
-		placeImage0 = (ImageView) getView().findViewById(R.id.placeImage0);
-		placeImage1 = (ImageView) getView().findViewById(R.id.placeImage1);
-		placeImage2 = (ImageView) getView().findViewById(R.id.placeImage2);
-
-		categoryChoice = (RadioGroup) getView().findViewById(
-				R.id.categoryChoice);
-
-		List<PlaceCategory> categories = new ArrayList<PlaceCategory>(
-				EnumSet.allOf(PlaceCategory.class));
-
-		List<RadioButton> catButtons = new ArrayList<RadioButton>();
-
-		for (PlaceCategory category : categories) {
-			RadioButton button = new RadioButton(getActivity());
-			button.setText(category.toString());
-			catButtons.add(button);
-			categoryChoice.addView(button);
-		}
-
-		chosenPlace[0].setText(places[0].getName());
-		chosenPlace[1].setText(places[1].getName());
-		chosenPlace[2].setText(places[2].getName());
-
-		RadioGroup placesGroup = (RadioGroup) getView().findViewById(
-				R.id.placesGroup);
-
-		placesGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(RadioGroup group, int checkedId) {
-				for (int i = 0; i < 3; i++)
-					if (checkedId == chosenPlace[i].getId())
-						place = places[i];
-			}
-		});
-
-		
-		  /*placeImage0.setImage(places[i]());
-		  placeImage1.setImage(place1.getName());
-		  placeImage2.setImage(place2.getName());*/
-		 
-		return view;
+	public static GalleryPickerFragment newInstance(int offset) {
+		GalleryPickerFragment localGalleryPickerFragment = new GalleryPickerFragment();
+		Bundle localBundle = new Bundle();
+		localBundle.putInt("offset", offset);
+		localGalleryPickerFragment.setArguments(localBundle);
+		return localGalleryPickerFragment;
 	}
 
 	public PlaceData getSelectedPlace() {
-		return place;
+		if (ownPlan.isChecked()) {
+			return null;
+		}
+		return places[lastChecked];
 	}
 
-	private PlaceData[] choosePlaces(Session session) {
-		PlaceCategory[] placecat = SessionHelper
-				.getCategoriesForSession(session);
-		DatabaseQuery plcat = new CategoryQuery(Arrays.asList(placecat));
-		DatabaseQuery open = new OpenAtQuery(
-				SessionHelper.getStartTimeForSession(session));
-		DatabaseQuery and = new AndQuery(plcat, open);
-		List<Integer> placeIds = MainScreenActivity.getPlacesDatabase().query(
-				and, new NameSorter(SortOrder.ASC));
-		List<PlaceData> places = new ArrayList<PlaceData>();
-		for (Integer placeId : placeIds)
-			places.add(MainScreenActivity.getPlacesDatabase().getPlaceByID(
-					placeId));
-		PlaceData[] chosenPlaces = choosePlaces(places);
-		return chosenPlaces;
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(2130903046, container, false);
+		offset = getArguments().getInt("offset");
+		Log.d("Joshua", "Get offset from args");
+		placeImages[0] = ((ImageView) view.findViewById(2131492892));
+		placeImages[1] = ((ImageView) view.findViewById(2131492894));
+		placeImages[2] = ((ImageView) view.findViewById(2131492896));
+		Log.d("Joshua", "Get text descriptions");
+		chosenPlace[0] = ((RadioButton) view.findViewById(2131492893));
+		chosenPlace[1] = ((RadioButton) view.findViewById(2131492895));
+		chosenPlace[2] = ((RadioButton) view.findViewById(2131492897));
+		Log.d("Joshua", "Get radio buttons");
+
+		View.OnClickListener placeClickedListener = new View.OnClickListener() {
+			public void onClick(View view) {
+				for (int i = 0; i < 3; i++) {
+					if ((view == chosenPlace[i]) || (view == placeImages[i])) {
+						chosenPlace[i].setChecked(true);
+						lastChecked = i;
+						chosenPlace[i].setChecked(false);
+					}
+				}
+			}
+		};
+
+		for (int i = 0; i < 3; i++) {
+			chosenPlace[i].setOnClickListener(placeClickedListener);
+			placeImages[i].setOnClickListener(placeClickedListener);
+
+		}
+		ArrayList<RadioButton> categoryRadioButtons;
+		categoryChoice = ((RadioGroup) view.findViewById(2131492890));
+		categoryChoice.setOrientation(RadioGroup.VERTICAL);
+		Log.d("Joshua", "Get category holder");
+		List<PlaceCategory> categoryList = new ArrayList<PlaceCategory>(
+				EnumSet.allOf(PlaceCategory.class));
+		Log.d("Joshua", "Create list of categories");
+		categoryRadioButtons = new ArrayList<RadioButton>();
+		Log.d("Joshua", "Create list of category buttons");
+		for (PlaceCategory cat : categoryList) {
+			if (cat != PlaceCategory.UNKNOWN) {
+				RadioButton catRadioButton = new RadioButton(getActivity());
+				catRadioButton.setText(cat.getName());
+				Log.d("Joshua", "Create category button for " + cat.toString());
+				categoryRadioButtons.add(catRadioButton);
+				categoryChoice.addView(catRadioButton);
+				Log.d("Joshua", "Add category button for " + cat.toString());
+			}
+		}
+
+		((RadioButton) categoryRadioButtons.get(0)).setChecked(true);
+		Log.d("Joshua", "Set checked boolean");
+		places = choosePlaces((PlaceCategory) categoryList.get(0));
+		Log.d("Joshua", "Create list of places");
+		for (int j = 0; j < 3; j++) {
+			chosenPlace[j].setText(places[j].getName());
+			Log.d("Joshua", "Set text for place");
+		}
+		ownPlan = ((CheckBox) view.findViewById(2131492898));
+		ownPlan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton checkBox,
+					boolean isChecked) {
+				for (int i = 0; i < 3; i++) {
+					chosenPlace[i].setEnabled(!isChecked);
+					placeImages[i].setEnabled(!isChecked);
+				}
+			}
+		});
+
+		return view;
 	}
 
-	private PlaceData[] choosePlaces(List<PlaceData> places) {
-		// TODO: Rewrite to better use list of places and/or not reuse places on
-		// the route.
-		PlaceData dummy = new PlaceData();
-		switch (places.size()) {
-		case 0:
-			return new PlaceData[] { dummy, dummy, dummy };
-		case 1:
-			return new PlaceData[] { places.get(0), dummy, dummy };
-		case 2:
-			return new PlaceData[] { places.get(0), places.get(1), dummy };
-		case 3:
-			return new PlaceData[] { places.get(0), places.get(1),
-					places.get(2) };
+	private PlaceData[] choosePlaces(List<PlaceData> paramList) {
+		// TODO: Improve this and clarify
+		PlaceData localPlaceData = new PlaceData();
+		switch (paramList.size()) {
 		default:
-			return new PlaceData[] { places.get(0), places.get(1),
-					places.get(2) };
+			PlaceData[] arrayOfPlaceData4 = new PlaceData[3];
+			arrayOfPlaceData4[0] = ((PlaceData) paramList.get(0));
+			arrayOfPlaceData4[1] = ((PlaceData) paramList.get(1));
+			arrayOfPlaceData4[2] = ((PlaceData) paramList.get(2));
+			return arrayOfPlaceData4;
+		case 0:
+			return new PlaceData[] { localPlaceData, localPlaceData,
+					localPlaceData };
+		case 1:
+			PlaceData[] arrayOfPlaceData3 = new PlaceData[3];
+			arrayOfPlaceData3[0] = ((PlaceData) paramList.get(0));
+			arrayOfPlaceData3[1] = localPlaceData;
+			arrayOfPlaceData3[2] = localPlaceData;
+			return arrayOfPlaceData3;
+		case 2:
+			PlaceData[] arrayOfPlaceData2 = new PlaceData[3];
+			arrayOfPlaceData2[0] = ((PlaceData) paramList.get(0));
+			arrayOfPlaceData2[1] = ((PlaceData) paramList.get(1));
+			arrayOfPlaceData2[2] = localPlaceData;
+			return arrayOfPlaceData2;
+		}
+	}
+
+	private PlaceData[] choosePlaces(PlaceCategory paramPlaceCategory) {
+		// TODO: clarify the contents of this method
+		Time localTime = new Time();
+		LocalTime localLocalTime = new LocalTime(localTime.year,
+				localTime.month, localTime.monthDay, localTime.hour + offset, 0);
+		AndQuery localAndQuery = new AndQuery(new CategoryQuery(
+				Collections.singletonList(paramPlaceCategory)),
+				new OpenAtQuery(localLocalTime));
+		List<?> localList = MainScreenActivity.getPlacesDatabase().query(
+				localAndQuery, new NameSorter(SortOrder.ASC));
+		ArrayList<PlaceData> localArrayList = new ArrayList<PlaceData>();
+		Iterator<?> localIterator = localList.iterator();
+		for (;;) {
+			if (!localIterator.hasNext()) {
+				return choosePlaces(localArrayList);
+			}
+			Integer localInteger = (Integer) localIterator.next();
+			localArrayList.add(MainScreenActivity.getPlacesDatabase()
+					.getPlaceByID(localInteger.intValue()));
 		}
 	}
 }
