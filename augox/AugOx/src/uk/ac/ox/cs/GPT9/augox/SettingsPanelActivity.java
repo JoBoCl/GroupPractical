3e6c980a5082df1b53696a23e71854b2f41f3ebe
Stephen <stephenheap1@hotmail.com>
2014-05-05 22:24:18 +0100

    Added Max Distance for AR View setting

--- augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SettingsPanelActivity.java
+++ augox/AugOx/src/uk/ac/ox/cs/GPT9/augox/SettingsPanelActivity.java
@@ -65,18 +65,6 @@ public class SettingsPanelActivity extends PreferenceActivity {
 		// Add 'general' preferences.
 		addPreferencesFromResource(R.xml.pref_general);
 
-		// Add 'notifications' preferences, and a corresponding header.
-		PreferenceCategory fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_notifications);
-		getPreferenceScreen().addPreference(fakeHeader);
-		addPreferencesFromResource(R.xml.pref_notification);
-
-		// Add 'data and sync' preferences, and a corresponding header.
-		fakeHeader = new PreferenceCategory(this);
-		fakeHeader.setTitle(R.string.pref_header_data_sync);
-		getPreferenceScreen().addPreference(fakeHeader);
-		addPreferencesFromResource(R.xml.pref_data_sync);
-
 		// Bind the summaries of EditText/List/Dialog/Ringtone preferences to
 		// their values. When their values change, their summaries are updated
 		// to reflect the new value, per the Android Design guidelines.
@@ -143,29 +131,6 @@ public class SettingsPanelActivity extends PreferenceActivity {
 						.setSummary(index >= 0 ? listPreference.getEntries()[index]
 								: null);
 
-			} else if (preference instanceof RingtonePreference) {
-				// For ringtone preferences, look up the correct display value
-				// using RingtoneManager.
-				if (TextUtils.isEmpty(stringValue)) {
-					// Empty values correspond to 'silent' (no ringtone).
-					preference.setSummary(R.string.pref_ringtone_silent);
-
-				} else {
-					Ringtone ringtone = RingtoneManager.getRingtone(
-							preference.getContext(), Uri.parse(stringValue));
-
-					if (ringtone == null) {
-						// Clear the summary if there was a lookup error.
-						preference.setSummary(null);
-					} else {
-						// Set the summary to reflect the new ringtone display
-						// name.
-						String name = ringtone
-								.getTitle(preference.getContext());
-						preference.setSummary(name);
-					}
-				}
-
 			} else {
 				// For all other preferences, set the summary to the value's
 				// simple string representation.
@@ -209,6 +174,7 @@ public class SettingsPanelActivity extends PreferenceActivity {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.pref_general);
 			bindPreferenceSummaryToValue(findPreference("setting_autoroute_max_length"));
+			bindPreferenceSummaryToValue(findPreference("setting_arview_max_distance"));
 		}
 	}
 }
